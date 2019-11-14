e2b9d59c954057b6c1d94d8d8942b9e6d91619ae
taterntots <matthew.a.weidner@gmail.com>
2019-11-14 16:03:54 -0500

    set up a validation schema for all fields. texbox not working right, though.

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -3,16 +3,19 @@ import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-function UserForm() {
-    // const [user, setUser] = useState({name: ''})
+function UserForm({ values, errors, touched }) {
+    // const [users, setUsers] = useState({name: ''})
 
     return (
         <div className='user-form'>
             <Form>
                 <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && <p>{errors.name}</p>}
                 <Field type="text" name="email" placeholder="Email" />
+                {touched.email && errors.email && <p>{errors.email}</p>}
                 <Field type="password" name="password" placeholder="Password" />
-                <Field type="checkbox" name="terms" checked={false}/>
+                {touched.password && errors.password && <p>{errors.password}</p>}
+                <Field type="checkbox" name="terms" checked={values.terms}/>
                 <button>Submit!</button>
             </Form>
         </div>
@@ -25,9 +28,28 @@ const FormikUserForm = withFormik({
             name: name || '',
             email: email || '',
             password: password || '',
-            terms: terms || ''
+            terms: terms || false
         };
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            .max(10)
+            .required(),
+        email: Yup.string()
+            .email()
+            .required(),
+        password: Yup.string()
+            .min(6)
+            .required(),
+        terms: Yup.bool()
+            .oneOf([true], 'Must Accept Terms of Service')
+    }),
+
+    handleSubmit(values) {
+        console.log(values);
     }
+
 })(UserForm);
 
 export default FormikUserForm;
